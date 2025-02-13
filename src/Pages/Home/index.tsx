3cc833d328710daa6f355dd2b5ab7752b4ba02b5
brenddonanjos <brenddon.dev@gmail.com>
2025-02-13 16:41:34 -0300

    [hotfix]: Hello Workd

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <HomeContainer>
       <h1>Face Recall</h1>
-      <h2>Hello World!</h2>
-      <p>I am the Face Recall, and I am here to help you!</p>
+      <h2>Olá mundo!</h2>
+      <p>Eu sou o Face Recall e estou aqui pra ajudá-lo!</p>
     </HomeContainer>
   );
 }
