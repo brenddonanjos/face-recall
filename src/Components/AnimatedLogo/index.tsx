d82ddb3282580489f945bd3e06d102be1b2979fd
brenddonanjos <brenddon.dev@gmail.com>
2025-02-13 17:16:56 -0300

    [feature]: Animated logo

---
+++ src/Components/AnimatedLogo/index.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+import logo from "../../Images/logo.png";
+import logoSmile from "../../Images/logo-smile.png";
+import logoBlink from "../../Images/logo-blink.png";
+import { useEffect, useState } from "react";
+
+const LogoImg = styled.img`
+  height: 60px;
+  width: 60px;
+  align-self: center;
+  transition: 0.4s ease-in-out;
+  cursor: pointer;
+  animation: rotate 2s ease-in-out 2;
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const images = [logo, logoSmile, logoBlink, logoSmile, logo];
+
+const AnimatedLogo = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 1000); // Troca de imagem a cada 1 segundo
+    return () => clearInterval(interval);
+  }, []);
+
+  return <LogoImg src={images[currentImage]} alt="Robô" />;
+};
+
+export default AnimatedLogo;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,16 +1,17 @@
-
 import styled from "styled-components";
+import AnimatedLogo from "../../Components/AnimatedLogo";
 
-const HomeContainer = styled.div `
+const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
   justify-content: center;
-`
+`;
 
 export default function Home() {
   return (
     <HomeContainer>
+      <AnimatedLogo />
       <h1>Face Recall</h1>
       <h2>Olá mundo!</h2>
       <p>Eu sou o Face Recall e estou aqui pra ajudá-lo!</p>
